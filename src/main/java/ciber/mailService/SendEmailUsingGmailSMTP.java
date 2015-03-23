27ddf1e200149bee776983f8387c7b7e1715a6e3
mathiamo <mathiamo@gmail.com>
2015-03-23 12:23:23 +0100

    Deploy

--- src/main/java/ciber/mailService/SendEmailUsingGmailSMTP.java
+++ src/main/java/ciber/mailService/SendEmailUsingGmailSMTP.java
@@ -4,9 +4,7 @@ package ciber.mailService;
  * Created by matmoe on 18.03.2015.
  */
 
-
 import com.google.gson.Gson;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Request;
