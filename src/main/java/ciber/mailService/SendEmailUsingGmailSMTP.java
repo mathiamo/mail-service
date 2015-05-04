03f33e45e74ecb417942e00478308de6676cbfaf
oyvindBjerke <o.t.bjerke@gmail.com>
2015-05-04 14:52:54 +0200

    Removed unused imports and hello world mapping

--- src/main/java/ciber/mailService/SendEmailUsingGmailSMTP.java
+++ src/main/java/ciber/mailService/SendEmailUsingGmailSMTP.java
@@ -7,20 +7,10 @@ package ciber.mailService;
 import ciber.mailService.dto.Mail;
 import ciber.mailService.service.MailService;
 import ciber.mailService.util.ConversionUtil;
-import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
-import java.util.Properties;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
 
 import static spark.Spark.*;
 
@@ -32,7 +22,6 @@ public class SendEmailUsingGmailSMTP {
         try {
             port(Integer.parseInt(System.getenv("PORT")));
             before((request, response) -> response.type("application/json"));
-            get("/", (request, response) -> "hello world");
             post("/sendMail", (Request request, Response response) -> {
                 String body = request.body();
                 Mail mail = ConversionUtil.convertJsonToMail(body);
